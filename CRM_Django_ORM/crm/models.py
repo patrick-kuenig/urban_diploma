45d8aa0670e8548d7b60f86cd02c46f844db0504
patrick-kuenig <kuenig.patrick@gmail.com>
2024-11-12 01:07:10 +0300

    changes

--- CRM_Django_ORM/crm/models.py
+++ CRM_Django_ORM/crm/models.py
@@ -5,6 +5,7 @@ from django.db.models.functions import Now
 
 class Category(models.Model):
     issue_type = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.issue_type
